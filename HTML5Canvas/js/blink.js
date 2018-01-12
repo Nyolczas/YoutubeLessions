add2b6446fdd6253f496bf10fdc98395be45a32a
Nyolczas <nyolczas@gmail.com>
2018-01-13 00:04:40 +0100

    blink

---
+++ HTML5Canvas/js/blink.js
@@ -0,0 +1,7 @@
+var canvas = document.querySelector('#blink-canvas');
+        var W = window.innerWidth;
+        var H = window.innerHeight;
+        canvas.width = W;
+        canvas.height = H;
+        var c = canvas.getContext('2d');
+        canvas.style.background = "pink";
