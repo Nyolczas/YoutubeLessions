45072a3847319c8b5d1842cc15f03c7c492cdb5c
Nyolczas <nyolczas@gmail.com>
2018-01-13 11:07:22 +0100

    blink

--- HTML5Canvas/js/blink.js
+++ HTML5Canvas/js/blink.js
@@ -16,8 +16,8 @@ function eyelid() {
     this.suly = W / 4;
     this.suly2 = W - this.suly;
     this.nyitott = H;
-    this.meretX = 0.5;
-    this.meretY = 0.5;
+    this.meretX = 1;
+    this.meretY = 1;
 
     this.rajzol = function(ctx) {
         ctx.save();
@@ -44,7 +44,29 @@ function eyelid() {
 
 // ================ rajzolás ================ 
 window.onload = function () {
+    var blinkX = W / 2;
+    var blinkY = H / 2;
+    var blinkR = 0;
+    var blinkSx = 0.5;
+    var blinkSy = 0.5;
+    var blinkOpen = H;
+
     var lid1 = new eyelid();
-    // lid1.y = H / 2;
+    lid1.x = blinkX;
+    lid1.y = blinkY;
+    lid1.forgatas = blinkR;
+    lid1.meretX = blinkSx;
+    lid1.meretY = blinkSy;
+    lid1.nyitott = blinkOpen;
     lid1.rajzol(ctx);
+
+    var lid2 = new eyelid();
+    lid2.x = blinkX;
+    lid2.y = blinkY;
+    lid2.forgatas = blinkR + 360;
+    lid2.meretX = blinkSx;
+    lid2.meretY = blinkSy;
+    lid2.nyitott = blinkOpen;
+
+    lid2.rajzol(ctx);
 } 
